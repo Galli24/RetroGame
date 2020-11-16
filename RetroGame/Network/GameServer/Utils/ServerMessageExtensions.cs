caba76718ae6467fc87f5d1c97bbd9d48cada326
Connor Gallimore <connor.gallimore@epitech.eu>
2020-11-16 19:59:39 +0100

    Extension to ServerMessage to add a .Send() function

---
+++ RetroGame/Network/GameServer/Utils/ServerMessageExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GameServer.Server;
+using LibNetworking.Messages.Server;
+
+namespace GameServer.Utils
+{
+    public static class ServerMessageExtensions
+    {
+        public static void Send(this ServerMessage serverMessage)
+        {
+            TCPServer.SendServerMessage(serverMessage.Destination, serverMessage);
+        }
+    }
+}
