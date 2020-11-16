b1713f3139383eb6ddebd05476f4d6ef70194979
Connor Gallimore <connor.gallimore@epitech.eu>
2020-11-16 20:01:24 +0100

    Added a simple WPF client to test server code

---
+++ RetroGame/Network/ServerTesting/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using LibNetworking;
+using LibNetworking.Messages.Client;
+using LibNetworking.Messages.Server;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Windows;
+
+
+namespace ServerTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly TCPClient _tcpClient;
+        private static readonly HttpClient _client = new HttpClient();
+
+        public MainWindow()
+        {
+            _tcpClient = new TCPClient("127.0.0.1", 27015);
+            NetworkCallbacks.OnServerMessage += OnServerMessage;
+
+            InitializeComponent();
+        }
+
+        private void OnServerMessage(ServerMessage message)
+        {
+            switch (message.ServerMessageType)
+            {
+                case ServerMessageType.CONNECT:
+                    var connectMessage = (ServerConnectMessage)message;
+                    if (connectMessage.Authorized)
+                    {
+                        Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Render, (System.Action)delegate ()
+                        {
+                            Error.Text = "Connected";
+                            button_connect.Visibility = Visibility.Visible;
+                        });
+                    } else
+                    {
+                        Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Render, (System.Action)delegate ()
+                        {
+                            Error.Text = connectMessage.Reason;
+                            button_connect.Visibility = Visibility.Visible;
+                        });
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async void Connect(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Error.Text = "Connecting...";
+                button_connect.Visibility = Visibility.Hidden;
+                var username = UsernameBox.Text;
+                var password = PasswordBox.Text;
+
+                // Auth Server
+                var uri = "https://localhost:5001/api/v1/users/authenticate";
+                var body = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
+
+                int statusCode = -1;
+                string responseData = string.Empty;
+
+                using var content = new StringContent(body, System.Text.Encoding.UTF8, "application/json");
+                using var response = await _client.PostAsync(uri, content).ConfigureAwait(false);
+                statusCode = (int)response.StatusCode;
+                responseData = await response.Content.ReadAsStringAsync();
+
+                if (statusCode == 200)
+                {
+                    var jsonData = JObject.Parse(responseData);
+                    var id = jsonData["id"].ToString();
+                    var token = jsonData["token"].ToString();
+
+                    _tcpClient.Connect();
+                    _tcpClient.SendClientMessage(new ClientConnectMessage(token, id, username));
+                } else
+                {
+                    var jsonData = JObject.Parse(responseData);
+                    var error = jsonData["error"].ToString();
+
+                    await Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Render, (System.Action)delegate ()
+                    {
+                        Error.Text = error;
+                        button_connect.Visibility = Visibility.Visible;
+                    });
+                }
+
+            } catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+                Trace.WriteLine(ex.InnerException);
+                Trace.WriteLine(ex.StackTrace);
+                await Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Render, (System.Action)delegate ()
+                 {
+                     Error.Text = "An internal error occured";
+                     button_connect.Visibility = Visibility.Visible;
+                 });
+            }
+        }
+    }
+}
---
+++ RetroGame/Network/ServerTesting/TCPClient.cs
@@ -0,0 +1,171 @@
+﻿using LibNetworking;
+using LibNetworking.Messages;
+using LibNetworking.Messages.Client;
+using LibNetworking.Messages.Server;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerTesting
+{
+    public sealed class TCPClient
+    {
+
+        #region Members
+
+        private readonly IPAddress _serverIP;
+        private readonly ushort _serverPort;
+        private Socket _socket;
+
+        private class SocketState
+        {
+            public Socket Socket;
+            public byte[] SizeBuffer;
+            public byte[] Buffer;
+            public MemoryStream Data;
+        }
+
+        #endregion
+
+        #region Logic
+
+        public TCPClient(string ip, ushort serverPort)
+        {
+            _serverIP = IPAddress.Parse(ip);
+            _serverPort = serverPort;
+        }
+
+        private void Close()
+        {
+            if (_socket != null)
+            {
+                _socket.Shutdown(SocketShutdown.Both);
+                _socket.Close();
+                _socket = null;
+            }
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                if (_socket != null)
+                {
+                    Close();
+                }
+
+                string localHostName = Dns.GetHostName();
+                IPHostEntry localMachineInfo = Dns.GetHostEntry(localHostName);
+                IPEndPoint serverEndpoint = new IPEndPoint(_serverIP, _serverPort);
+                IPEndPoint myEndpoint = new IPEndPoint(IPAddress.Any, 0);
+
+                _socket = new Socket(myEndpoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _socket.Connect(serverEndpoint);
+                Trace.WriteLine("Server connection established");
+
+                Receive(_socket);
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+                Trace.WriteLine(e.StackTrace);
+            }
+        }
+
+        public void SendClientMessage(ClientMessage message)
+        {
+            if (_socket != null)
+            {
+                Trace.WriteLine($"Sending a packet with a size of {Message.SerializeToBytes(message).Length} bytes to {_socket.RemoteEndPoint}");
+
+                var serializedMessage = Message.SerializeToBytes(message);
+                var sizeBytes = BitConverter.GetBytes(serializedMessage.Length);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(sizeBytes);
+
+                _socket.Send(sizeBytes);
+                _socket.Send(serializedMessage);
+            }
+        }
+
+        private void Receive(Socket socket)
+        {
+            if (_socket == null)
+                return;
+
+            var state = new SocketState();
+
+            try
+            {
+                state.Socket = socket;
+                state.SizeBuffer = new byte[4];
+
+                state.Socket.BeginReceive(state.SizeBuffer, 0, 4, SocketFlags.None, new AsyncCallback(OnReceivePacketSize), state);
+            }
+            catch
+            {
+                Close();
+            }
+        }
+
+        private void OnReceivePacketSize(IAsyncResult result)
+        {
+            var state = (SocketState)result.AsyncState;
+            var socket = state.Socket;
+
+            try
+            {
+                int readBytes = socket.EndReceive(result);
+                if (readBytes == 4)
+                {
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(state.SizeBuffer);
+
+                    var packetSize = BitConverter.ToInt32(state.SizeBuffer);
+                    state.Buffer = new byte[packetSize + 1];
+                    socket.BeginReceive(state.Buffer, 0, packetSize, SocketFlags.None, new AsyncCallback(OnReceivePacket), state);
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            catch
+            {
+                Close();
+            }
+        }
+
+        private void OnReceivePacket(IAsyncResult result)
+        {
+            var state = (SocketState)result.AsyncState;
+            var socket = state.Socket;
+
+            try
+            {
+                int readBytes = socket.EndReceive(result);
+                if (readBytes > 0)
+                {
+                    state.Data = new MemoryStream(state.Buffer, 0, readBytes, false, true);
+                    var message = Message.DeserializeFromStream(state.Data);
+                    if (message.MessageType == MessageType.SERVER)
+                        NetworkCallbacks.OnServerMessage((ServerMessage)message);
+                    state.SizeBuffer = new byte[4];
+                    state.Socket.BeginReceive(state.SizeBuffer, 0, 4, SocketFlags.None, new AsyncCallback(OnReceivePacketSize), state);
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            catch
+            {
+                Close();
+            }
+        }
+
+        #endregion
+    }
+}
